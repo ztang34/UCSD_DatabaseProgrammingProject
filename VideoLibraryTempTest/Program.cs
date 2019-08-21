78144aa2f35da05a273f01d6a631394280eb3850
Zhenguan Tang <ztang@osisoft.com>
2019-08-20 22:23:47 -0700

    Added VideoLibraryTempTest Project

---
+++ VideoLibraryTempTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCSD.VideoLibrary;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace VideoLibraryTempTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory());
+            var dal = new EntLibDAL();
+
+            var criteria = new VideoSearchCriteria();
+            criteria.Year = -1;
+
+            var results = dal.SearchVideoLibrary(criteria);
+
+            foreach(VideoSearchResult r in results)
+            {
+                Console.WriteLine($"{r.VideoId}, {r.Title}, {r.Year}, {r.Director}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
